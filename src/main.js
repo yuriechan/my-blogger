4856252db254355f2d786de3a118341d1176bfc5
Yurie <cutecupcakeandcute@gmail.com>
2020-06-27 00:41:53 +0900

    add firebase config in .env & use the variable for firebase config data

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,

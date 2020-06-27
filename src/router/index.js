b7a6da6d2e530f7c5bdbaf50dc9987d91ebce0d4
Yurie <cutecupcakeandcute@gmail.com>
2020-06-27 13:52:29 +0900

    remove default components and update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Gallery from "../components/Gallery.vue";
 import CreateBlog from "../views/CreateBlog.vue";
 
 Vue.use(VueRouter);
@@ -8,8 +8,8 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Gallery",
+    component: Gallery
   },
   {
     path: "/CreateBlog",

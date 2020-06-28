4620b4a1f20ec4442c0e226b0951bb00eca80c13
Yurie <cutecupcakeandcute@gmail.com>
2020-06-29 02:04:43 +0900

    add contenteditable attribute & save btn view

--- src/components/BlogDetail.vue
+++ src/components/BlogDetail.vue
@@ -1,12 +1,18 @@
 <template>
-  <v-card>
+  <v-card
+    :style="
+      editing ? { backgroundColor: '#BBDEFB' } : { backgroundColor: '#FFFFFF' }
+    "
+  >
     <v-img class="white--text" height="200px" :src="this.postDefaultImg">
     </v-img>
     <div class="px-16">
-      <v-card-title class="pb-10">
+      <v-card-title class="pb-10" :contenteditable="editing" @input="editTitle">
         <span class="headline">{{ this.postTitle }}</span>
       </v-card-title>
-      <v-card-text>{{ this.postContent }}</v-card-text>
+      <v-card-text :contenteditable="editing" @input="editBody">{{
+        this.postContent
+      }}</v-card-text>
     </div>
     <v-card-actions>
       <v-spacer></v-spacer>
@@ -24,7 +30,7 @@
             <v-icon v-else>mdi-account-circle</v-icon>
           </v-btn>
         </template>
-        <v-btn fab dark small color="green">
+        <v-btn fab dark small color="green" @click="editing = !editing">
           <v-icon>mdi-pencil</v-icon>
         </v-btn>
         <v-btn
@@ -37,6 +43,9 @@
           <v-icon>mdi-delete</v-icon>
         </v-btn>
       </v-speed-dial>
+      <v-btn class="ma-2 saveBtn" outlined color="indigo"
+        >Outlined Button</v-btn
+      >
     </v-card-actions>
     <v-dialog v-model="confirmDialogOpen" max-width="500px">
       <v-card>
@@ -72,7 +81,11 @@ export default {
     right: true,
     bottom: true,
     transition: "slide-y-reverse-transition",
-    confirmDialogOpen: false
+    confirmDialogOpen: false,
+    titleText: "",
+    bodyText: "",
+    editing: false,
+    saveBtnOpen: false
   }),
   methods: {
     deletePost() {
@@ -86,7 +99,43 @@ export default {
           this.$router.go();
         })
         .catch(err => console.log("Error getting documents", err));
+    },
+    editTitle(e) {
+      this.titleText = e.target.innerText;
+    },
+    editBody(e) {
+      this.bodyText = e.target.innerText;
+    },
+    textChanged(originalText, newText) {
+      return originalText.localeCompare(newText);
     }
+    // Did not work as expected, might use later.
+    // focusTitleInput() {
+    //   console.log("focus");
+    //   console.log(this.$refs.editTitle);
+    //   this.$refs.editTitle.focus();
+    // },
+    // focusBodyInput() {
+    //   console.log("focus");
+    //   console.log(this.$refs.editBody);
+    //   this.$refs.editBody.focus();
+    // }
+  },
+  watch: {
+    // editing: function(val) {
+    //   console.log(val);
+    //   if (val) {
+    //     this.focusTitleInput();
+    //     this.focusBodyInput();
+    //   }
+    // }
   }
 };
 </script>
+<style scoped>
+.saveBtn {
+  left: 16px;
+  position: absolute;
+  bottom: 16px;
+}</style
+>>

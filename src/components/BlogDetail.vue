3dc4fa282c068aa4c08fafdb33d74ca8d5788a61
Yurie <cutecupcakeandcute@gmail.com>
2020-06-28 23:01:58 +0900

    add delete confirmation view and delete method to the db

--- src/components/BlogDetail.vue
+++ src/components/BlogDetail.vue
@@ -27,24 +27,66 @@
         <v-btn fab dark small color="green">
           <v-icon>mdi-pencil</v-icon>
         </v-btn>
-        <v-btn fab dark small color="red">
+        <v-btn
+          fab
+          dark
+          small
+          color="red"
+          @click="confirmDialogOpen = !confirmDialogOpen"
+        >
           <v-icon>mdi-delete</v-icon>
         </v-btn>
       </v-speed-dial>
     </v-card-actions>
+    <v-dialog v-model="confirmDialogOpen" max-width="500px">
+      <v-card>
+        <v-card-title class="headline"
+          >Are you sure you want to delete this post?</v-card-title
+        >
+        <v-card-text>Deleted post will never be recovered.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="(confirmDialogOpen = false), deletePost()"
+            >Yes, delete.</v-btn
+          >
+          <v-btn color="green darken-1" text @click="confirmDialogOpen = false"
+            >No, I changed my mind.</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 <script>
+import db from "../../firebaseConfig";
 export default {
   name: "BlogDetail",
-  props: ["postTitle", "postContent", "postDefaultImg"],
+  props: ["postTitle", "postContent", "postDefaultImg", "postId", "postAutoId"],
   data: () => ({
     direction: "left",
     fab: false,
     hover: true,
     right: true,
     bottom: true,
-    transition: "slide-y-reverse-transition"
-  })
+    transition: "slide-y-reverse-transition",
+    confirmDialogOpen: false
+  }),
+  methods: {
+    deletePost() {
+      db.collection("blog")
+        .where("post_id", "==", this.$route.query.id)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            doc.ref.delete();
+          });
+          this.$router.go();
+        })
+        .catch(err => console.log("Error getting documents", err));
+    }
+  }
 };
 </script>

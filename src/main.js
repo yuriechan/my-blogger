497338c48de6fbbe706caa3c2ee50aa042cfa117
Yurie <cutecupcakeandcute@gmail.com>
2020-06-30 17:02:01 +0900

    remove vuelidate from import since it is unused

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import vuelidate from "vuelidate";
 
 Vue.config.productionTip = false;
 
@@ -11,6 +10,5 @@ new Vue({
   router,
   store,
   vuetify,
-  vuelidate,
   render: h => h(App)
 }).$mount("#app");
